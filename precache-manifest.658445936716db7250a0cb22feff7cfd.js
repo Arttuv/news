becd7beaa18792f495b8d60aad8a09cfc769ae88
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2020-04-19 00:15:33 +0300

    Updates

---
+++ precache-manifest.658445936716db7250a0cb22feff7cfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ab34a3f7b94f45c78de20331f4649a",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "224180cce480f1b40a63",
+    "url": "/news/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5dbde434e755a44927b7",
+    "url": "/news/static/js/2.8826b2b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news/static/js/2.8826b2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224180cce480f1b40a63",
+    "url": "/news/static/js/main.3d129572.chunk.js"
+  },
+  {
+    "revision": "99d5e975076441976450",
+    "url": "/news/static/js/runtime-main.725caa6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news/static/media/logo.5d5d9eef.svg"
+  }
+]);
